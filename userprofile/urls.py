3943a0a00e835c3ab4a99deea766a9bc6c464bed
izzat1998 <tiran.sss@mail.ru>
2019-08-13 12:10:00 +0000

    now post is working

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from userprofile.views import Home, UserPage, PostComment, Login, CreatePost
+from userprofile.views import Home, UserPage, Login, CreatePost, CreateComment
 
 urlpatterns = [
 
     path('login/', Login.as_view(), name='login'),
-    path('comment/create/<int:pk>', PostComment.as_view(), name='post_comment'),
+    path('comment/create/', CreateComment.as_view(), name='post_comment'),
     path('create/post', CreatePost.as_view(), name='create_post'),
     path('<str:username>', UserPage.as_view(), name='user_page'),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,15 +1,19 @@
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
+from django.http.response import JsonResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
+from requests import Response
+from rest_framework.generics import CreateAPIView
+from rest_framework.serializers import ModelSerializer
 
 from post.models import Post, Comment
-from userprofile.models import UserProfile, Friend, Requests
+from userprofile.models import UserProfile, Friend
 
 
 class Login(View):
@@ -33,40 +37,58 @@ class UserPage(View):
         friends = UserProfile.objects.all()
         return render(request, 'userprofile/user_page.html', context={'user': current_user, 'posts': posts, 'friends': friends})
 
-    def post(self, request, username):
-        # CreatePost
-        content = request.POST.get('post_content')
-        current_user = UserProfile.objects.get(user=request.user)
-        Post.objects.create(body=content, author=current_user)
-        posts = Post.objects.filter(author=current_user)
-        friends = UserProfile.objects.all()
-        return render(request, 'userprofile/user_page.html', context={'user': current_user, 'posts': posts, 'friends': friends})
+
+class PostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('body',)
 
 
-class CreateRequest(View):
-    def post(self, request):
-        r_id = request.POST.get("r_id")
-        s_id = request.POST.get("s_id")
-        Requests.objects.create(sender_id=s_id, receiver_id=r_id)
-        return HttpResponse()
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('content', 'post')
+
+
+class CreatePost(CreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        instance.author = self.request.user.userprofile
+        instance.save()
+
+
+# class CreatePost(View):
+#     def post(self, request):
+#         content = request.POST.get('content')
+#         current_user = UserProfile.objects.get(user=request.user)
+#         post = Post.objects.create(body=content, author=current_user)
+#         return HttpResponseRedirect(reverse('user_page', kwargs={'username': user.username}))
+#
+
+class CreateComment(CreateAPIView):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        instance.author = self.request.user.userprofile
+        instance.post = self.request.POST.get('post_id')
+        instance.save()
 
 
 class SendRequest(View):
-
     def get(self, request, pk):
-        try:
-            requested_be_friend = UserProfile.objects.get(pk=pk)
-            current_user = UserProfile.objects.get(user=request.user)
-            current_user.sended_requests.create()
-            return HttpResponseRedirect(reverse('user_page', kwargs={'username': request.user.username}))
-        except:
-            return HttpResponse('Error in send_request')
+        return HttpResponseRedirect(reverse('user_page', kwargs={'username': request.user.username}))
 
 
 class PostComment(View):
     def post(self, request, pk):
         content_comment = request.POST.get('post_comment')
         current_post = Post.objects.get(pk=pk)
+        print(current_post)
         current_user = UserProfile.objects.get(user=request.user)
         Comment.objects.create(content=content_comment, post=current_post, author=current_user)
         return HttpResponseRedirect(reverse('user_page', kwargs={'username': request.user.username}))
