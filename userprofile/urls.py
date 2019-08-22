2c62cd1ba0c24966ad11a7e8225a351072899792
Aziza <azibonnu19@gmail.com>
2019-08-22 16:11:34 +0500

    User page comment

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 from api.views import CreatePost, CreateComment, DestroyPost
-from post.views import PostDelete
 from userprofile.views import Home, UserPage, Login, UserFriends
 
 urlpatterns = [
@@ -12,5 +11,5 @@ urlpatterns = [
     path('create/post/', CreatePost.as_view(), name='create_post'),
     path('<str:username>/', UserPage.as_view(), name='user_page'),
     path('<str:username>/friends/', UserFriends.as_view(), name='user-friends'),
-    path('post/<int:pk>/delete/',DestroyPost.as_view(),name='post-delete'),
+    path('post/<int:pk>/delete/', DestroyPost.as_view(), name='post-delete'),
 ]
